cf6c8887eafa50b52e989f36d51faa1d63f84aae
Rajneesh Rathore <rajneeshrathore63@gmail.com>
2026-04-16 21:10:35 +0530

    bug fix

--- frontend/src/components/normal/Message.jsx
+++ frontend/src/components/normal/Message.jsx
@@ -9,23 +9,31 @@ const Message = ({ message }) => {
   //console.log("info log: ",message);
 
   return (
-    <div className={`flex gap-3 px-4 py-3 rounded-2xl w-fit max-w-[85%] ${
-      isOwnMessage 
-        ? "ml-auto bg-zinc-800 flex-row-reverse" 
-        : "mr-auto bg-white/[0.03] backdrop-blur-md border border-white/[0.05]"
-    } transition-all duration-300 hover:scale-[1.01]`}>
+    <div
+      className={`flex gap-3 px-4 py-3 rounded-2xl w-fit max-w-[85%] ${
+        isOwnMessage
+          ? "ml-auto bg-zinc-800 flex-row-reverse"
+          : "mr-auto bg-white/[0.03] backdrop-blur-md border border-white/[0.05]"
+      } transition-all duration-300 hover:scale-[1.01]`}
+    >
       <img
         src={optimizeAvatar(message.sender.avatarUrl)}
         alt="avatar"
         className="w-10 h-10 rounded-full object-cover"
       />
       <div className="flex flex-col max-w-[70%]">
-        <div className={`flex items-center gap-2 ${isOwnMessage ? 'flex-row-reverse' : ''}`}>
-          <span className={`font-semibold text-sm h-full ${isOwnMessage ? 'text-white' : 'text-zinc-100'}`}>
+        <div
+          className={`flex items-center gap-2 ${isOwnMessage ? "flex-row-reverse" : ""}`}
+        >
+          <span
+            className={`font-semibold text-sm h-full ${isOwnMessage ? "text-white" : "text-zinc-100"}`}
+          >
             {message.sender.username}
           </span>
 
-          <span className={`text-xs h-full whitespace-nowrap ${isOwnMessage ? 'text-zinc-400' : 'text-zinc-500'}`}>
+          <span
+            className={`text-xs h-full whitespace-nowrap ${isOwnMessage ? "text-zinc-400" : "text-zinc-500"}`}
+          >
             {new Date(message.createdAt).toLocaleTimeString([], {
               hour: "2-digit",
               minute: "2-digit",
@@ -34,18 +42,28 @@ const Message = ({ message }) => {
         </div>
 
         {message.content && (
-          <p className={`text-sm mt-1 leading-relaxed break-words ${isOwnMessage ? 'text-white' : 'text-zinc-300'} ${isOwnMessage ? 'text-right' : 'text-left'}`}>
+          <p
+            className={`text-sm mt-1 leading-relaxed break-words ${isOwnMessage ? "text-white" : "text-zinc-300"} ${isOwnMessage ? "text-right" : "text-left"}`}
+          >
             {message.content}
           </p>
         )}
 
-        {message.attachment.map((attachment, index) => (
-          <div className="mt-2" key={index}>
-            <img
-              src={attachment.url}
-              alt="attachment"
-              className="rounded-lg max-h-72 object-cover border border-zinc-800 shadow-md hover:scale-[1.02] transition-transform duration-300 cursor-pointer"
-            />
+        {message.attachment?.map((file, index) => (
+          <div key={index}>
+            {file.type === "image" && (
+              <img src={file.url} className="w-40 rounded p-2" />
+            )}
+
+            {file.type === "video" && (
+              <video src={file.url} controls className="w-40 rounded p-2" />
+            )}
+
+            {file.type === "file" && (
+              <a href={file.url} target="_blank">
+                📄 {file.fileName}
+              </a>
+            )}
           </div>
         ))}
       </div>
